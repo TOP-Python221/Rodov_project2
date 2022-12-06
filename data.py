0698897bf12b25d25a261b09027fb1949793ed58
Gennadiy <renardes@gmail.com>
2022-12-06 14:46:22 +0500

    пример чтения состояний ранее сохранённого питомца

---
+++ data.py
@@ -0,0 +1,50 @@
+# импорт из стандартной библиотеки
+from json import load as jload, dump as jdump
+from datetime import datetime as dt
+
+# импорт дополнительных модулей
+import states
+import constants
+
+
+class PersistenceManager:
+    """
+    Предоставляет пути по умолчанию и методы для работы с файлами данных.
+    """
+    default_parameters_path = constants.BASE_DIR / 'parameters.json'
+    default_states_path = constants.BASE_DIR / 'states.json'
+
+    @classmethod
+    def read_parameters(cls, kind: constants.Kind, parameters_path: constants.pathlike = None) -> states.KindParameters:
+        """Загружает из JSON файла параметры вида в экземпляр KindParameters и возвращает его."""
+
+    @classmethod
+    def read_states(cls, states_path: constants.pathlike = None) -> states.StatesManager:
+        """Загружает из JSON файла последнее сохранённое состояние питомца в экземпляр StatesManager и возвращает его."""
+        if not states_path:
+            states_path = cls.default_states_path
+        with open(states_path, encoding='utf-8') as filein:
+            data = jload(filein)
+
+        data['body_state']['timestamp'] = dt.strptime(data['body_state']['timestamp'], '%Y-%m-%d %H:%M:%S')
+        data['mind_state']['timestamp'] = dt.strptime(data['mind_state']['timestamp'], '%Y-%m-%d %H:%M:%S')
+
+        return states.StatesManager(
+            constants.Kind(data['kind']),
+            data['name'],
+            dt.strptime(data['birthdate'], '%Y-%m-%d %H:%M:%S'),
+            states.BodyState(**data['body_state']),
+            states.MindState(**data['mind_state'])
+        )
+
+    @classmethod
+    def write_states(cls, state, states_path: constants.pathlike = None):
+        """"""
+
+
+# тесты
+if __name__ == '__main__':
+    sm = PersistenceManager.read_states()
+    print(sm.__dict__, end='\n\n')
+    print(sm.body.__dict__, end='\n\n')
+    print(sm.mind.__dict__, end='\n\n')
--- states.py
+++ states.py
@@ -1,117 +1,44 @@
-from __future__ import annotations
+# импорт из стандартной библиотеки
+from datetime import datetime as dt
+from dataclasses import dataclass
 
-import datetime
-import pathlib
-from typing import List
-import main
-import datetime as dt
-import json
-
-# saves = {
-#     'health': ((1, 2), (3, 9), (10, 16), (17, 20),),
-#     'stamina': ((1, 3), (4, 10), (11, 17), (18, 20),),
-#     'hunger': ((1), (1), (1), (1),),
-#     'thirst': ((1), (1), (1), (1),),
-#
-# }
-
-class StateCalculator:
-
-    def __init__(self, previous: 'StatesManager'):
-        self.previous = previous
-
-    # КОММЕНТАРИЙ: именно эти методы ответственны за вычисления новых мгновенных значений параметров после загрузки данных из файла(-ов) состояний
-    def __new_body(self) -> main.Body:
-        pass
-
-    def __new_mind(self) -> main.Mind:
-        pass
-
-    def new_creature(self, new_name, new_birthdate):
-        from main import Creature # Избавляет от закальцованного импорта
-        self.new_name = new_name
-        self.new_birthdate = new_birthdate
-        return Creature(new_name, new_birthdate, self.__new_body(), self.__new_mind())
+# импорт дополнительных модулей
+import creature
+import constants
 
 
-class StatesManager:
+# КОММЕНТАРИЙ: располагайте классы в порядке использования: сначала самые вложенные, затем те, которые их используют, и так далее
+
+
+class Ranges:
+    """
+    Диапазоны параметров вида !! и влияния !!
+    """
     def __init__(self,
-                 kind: main.Kind,
-                 name: str,
-                 birthdate: dt,
-                 body_last: BodyState,
-                 mind_last: MindState,
+                 health: constants.ParamRanges,
+                 stamina: constants.ParamRanges,
+                 hunger: constants.ParamRanges,
+                 thirst: constants.ParamRanges,
                  ):
-        self.kind = kind
-        self.name = name
-        self.birthdate = birthdate
-        self.body_last = body_last
-        self.mind_last = mind_last
-
-
-
-class PersistenceManager:
-    def __init__(self, default_config_path: str | 'Path'):
-        # D:\Rodov_project2\Rodov_project2\states.py
-        self.default_config_path = default_config_path
-
-    # ИСПРАВИТЬ: в этом классе мы обычно прописываем не общие методы "чтения любого файла", а вполне конкретные, например: "читать json файл состояний", "читать ini файл конфигурации" (если такой будет) и так далее
-    # КОММЕНТАРИЙ: этот метод, например, должен возвращать объект StatesManager, то есть он читает именно состояния — это как раз случай, когда можно и нужно изменить модель в имени метода
-    @staticmethod
-    def read_file(filename):
-        """Чтение json файлов"""
-        with open(filename, 'r', encoding='utf-8') as file:
-            load_file = json.load(file)
-            name = load_file['cat']['title']
-            birthday = load_file['cat']['birthday']
-            return StatesManager(main.Kind.CAT, name, birthday)
-
-    @staticmethod
-    def write_file(save, filename):
-        save = json.dumps(save)
-        save = json.loads(str(save))
-        with open(filename, 'w', encoding='utf-8') as file:
-            json.dump(save, file, indent=4)
-
-# КОММЕНТАРИЙ: эти два класса никто не использует, им грустно и одиноко — поговорите с ними, вдруг пригодятся
-class BodyState:
-    def __init__(self,
-                 timestamp: dt,
-                 health: int,
-                 stamina: int,
-                 hunger: int,
-                 thirst: int):
-        self.timestamp = timestamp
         self.health = health
         self.stamina = stamina
         self.hunger = hunger
         self.thirst = thirst
 
 
-class MindState:
-    def __init__(self,
-                 timestamp: dt,
-                 joy: int,
-                 anger: int,
-                 pattern):
-        self.timestamp = timestamp
-        self.joy = joy
-        self.anger = anger
-        self.pattern = pattern
-
-
 class KindParameters:
+    """
+    Описывает параметры, диапазоны и взаимные влияния параметров для разных возрастных этапов вида питомцев.
+    """
     def __init__(self,
                  title: str,
                  maturity: tuple,
-                 egg: Ranges,
                  cub: Ranges,
                  yong: Ranges,
                  adult: Ranges,
                  elder: Ranges):
         self.title = title
         self.maturity = maturity
-        self.egg = egg
         self.cub = cub
         self.yong = yong
         self.adult = adult
@@ -121,24 +48,80 @@ class KindParameters:
         pass
 
 
-class Ranges:
+@dataclass
+class BodyState:
+    """
+    Снимок (хранитель) физиологических параметров питомца определённый момент.
+    """
+    timestamp: dt
+    health: int
+    stamina: int
+    hunger: int
+    thirst: int
+    intestine: int
+
+
+@dataclass
+class MindState:
+    """
+    Снимок (хранитель) эмоциональных параметров питомца определённый момент.
+    """
+    timestamp: dt
+    joy: int
+    activity: float
+    anger: int
+    anxiety: float
+
+
+
+class StatesManager:
+    """
+    Полностью описывает ранее сохранённого питомца.
+    """
     def __init__(self,
-                 health: 'ParamRanges',
-                 stamina: 'ParamRanges',
-                 hunger: 'ParamRanges',
-                 thirst: 'ParamRanges',
+                 kind: constants.Kind,
+                 name: str,
+                 birthdate: dt,
+                 body_last: BodyState,
+                 mind_last: MindState,
                  ):
-        self.health = health
-        self.stamina = stamina
-        self.hunger = hunger
-        self.thirst = thirst
+        self.kind = kind
+        self.name = name
+        self.birthdate = birthdate
+        self.body = body_last
+        self.mind = mind_last
+
+
+class StateCalculator:
+    """
+    Предоставляет методы для вычисления нового состояния питомца на основании ранее сохранённого.
+    ?? Предоставляет метод(-ы) для создания новорождённого питомца ??
+    """
+    def __init__(self, previous: StatesManager):
+        self.previous = previous
+
+    def __new_body(self) -> creature.Body:
+        pass
+
+    def __new_mind(self) -> creature.Mind:
+        pass
+
+    # ОТВЕТИТЬ: это метод создания экземпляра для уже существующего питомца или для новорождённого? в зависимости от этого у вас, очевидно, будут различные источники данных — да и сами действия отличаться будут скорее всего, ведь для уже существующего питомца надо ещё расчёты нового состояния провести
+    def new_creature(self, new_name, new_birthdate):
+        return creature.Creature(
+            new_name,
+            new_birthdate,
+            self.__new_body(),
+            self.__new_mind()
+        )
 
 
 # тесты:
 if __name__ == '__main__':
-    st = StatesManager('', '', '')
-    print(st)
-
-    # pm = PersistenceManager('')
-    # # pm.write_file(saves, 'property_saves.json')
-    # print(pm.read_file('property_saves.json'))
+    # saves = {
+    #     'health': ((1, 2), (3, 9), (10, 16), (17, 20),),
+    #     'stamina': ((1, 3), (4, 10), (11, 17), (18, 20),),
+    #     'hunger': ((1), (1), (1), (1),),
+    #     'thirst': ((1), (1), (1), (1),),
+    # }
+    pass
