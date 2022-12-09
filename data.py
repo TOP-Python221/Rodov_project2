f3217b6f534dc27d6adb0d566e77206d9cf81cff
RodovR <rodovroman@yandex.ru>
2022-12-09 19:08:08 +0300

    Реализация read_parameters(), попытка* реализации write_states()

--- data.py
+++ data.py
@@ -3,6 +3,7 @@ from json import load as jload, dump as jdump
 from datetime import datetime as dt
 
 # импорт дополнительных модулей
+import creature
 import states
 import constants
 
@@ -16,7 +17,20 @@ class PersistenceManager:
 
     @classmethod
     def read_parameters(cls, kind: constants.Kind, parameters_path: constants.pathlike = None) -> states.KindParameters:
+
         """Загружает из JSON файла параметры вида в экземпляр KindParameters и возвращает его."""
+        if not parameters_path:
+            parameters_path = cls.default_parameters_path
+        with open(parameters_path, encoding='utf-8') as filein:
+            data = jload(filein)
+
+        return states.KindParameters(data[kind]['title'],
+                                     data[kind]['maturation'],
+                                     False,
+                                     data[kind]['ranges']['cub'],
+                                     data[kind]['ranges']['young'],
+                                     data[kind]['ranges']['adult'],
+                                     data[kind]['ranges']['cub'])
 
     @classmethod
     def read_states(cls, states_path: constants.pathlike = None) -> states.StatesManager:
@@ -39,12 +53,18 @@ class PersistenceManager:
 
     @classmethod
     def write_states(cls, state, states_path: constants.pathlike = None):
-        """"""
+        # if not states_path:
+        #     states_path = cls.default_states_path
+        with open(states_path, 'w+', encoding='utf-8') as filein:
+            state = states.StatesManager(constants.Kind.CAT.value, 'Boris', '19.02.2020', '', '')
+            data = jdump(filein)
+
 
 
 # тесты
 if __name__ == '__main__':
     sm = PersistenceManager.read_states()
-    print(sm.__dict__, end='\n\n')
-    print(sm.body_last.__dict__, end='\n\n')
-    print(sm.mind_last.__dict__, end='\n\n')
+    print(sm.mind_last.__dict__['health'])
+    # print(sm.__dict__, end='\n\n')
+    # print(sm.body_last.__dict__, end='\n\n')
+    # print(sm.mind_last.__dict__, end='\n\n')
