0570854e456eea3f0734c29610b108b0a4654432
RodovR <rodovroman@yandex.ru>
2022-12-17 19:40:30 +0300

    Из референса: добавлен класс Controller

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from data import PersistenceManager
+import data
+import creature
+import states
+
+
+class Controller:
+    def __init__(self):
+        active_pet = data.PersistenceManager.read_states('states.json')
+        if active_pet:
+            states.StateCalculator.revive_creature()
 
 
 # КОММЕНТАРИЙ: жаль, что вы не услышали сообщение о том, что основная работа в этом проекте проходит с моделью и от модели — за одиннадцать коммитов ни одной правки в модель
