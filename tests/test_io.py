d60c1c30c38e7869c4d6c0a14ea1f8c6a2792f24
RodovR <rodovroman@yandex.ru>
2022-12-29 18:25:50 +0300

    Косметические изменения

--- tests/test_io.py
+++ tests/test_io.py
@@ -20,64 +20,84 @@ from src.model import states
 # ...
 
 
-# def get_init_param() -> list:
-#   """Берёт полный список атрибутов экземпляра класса и возвращает список переданных в конструктор значений"""
-#   new_atrs = []
-#   active_pet = PM()
-#   saved_pet = active_pet.read_parameters(constants.Kind.CAT).__dir__()
+def get_init_param() -> list:
+  """Берёт полный список атрибутов экземпляра класса и возвращает список переданных в конструктор значений"""
+  init_param = []
+  active_pet = PM()
+  saved_pet = active_pet.read_parameters(constants.Kind.CAT).__dir__()
+
+  for val in saved_pet:
+    if not val.startswith('__'):
+      init_param.append(val)
+
+  return init_param
+
+# @fixture()
+def take_empty_dict():
+    return {}
+
+# @fixture()
+def take_empty_list():
+    return []
+
+# @fixture()
+def take_empty_str():
+    return ''
+
+# @fixture()
+def take_int():
+    return 1
+
+# @fixture()
+def take_unex_params():
+    return ['unex_param', 'title', 'maturity', 'cub', 'young', 'adult', 'elder', 'Ranges', 'age_ranges']
+
+# @fixture()
+def take_less_params():
+    return ['maturity', 'cub', 'elder', 'Ranges', 'age_ranges']
+
+
+class TestActiveParameters:
+  @staticmethod
+  @mark.xfail
+  @mark.parametrize('arg, exp_res', [(take_empty_dict(), get_init_param()),
+                                     (take_empty_list(), get_init_param()),
+                                     (take_empty_str(), get_init_param()),
+                                     (take_int(), get_init_param()),
+                                     (take_unex_params(), get_init_param()),
+                                     (take_less_params(), get_init_param())])
+  def test_unhappy_keys(arg, exp_res):
+    assert arg == exp_res
+
+  @staticmethod
+  def test_happy_keys():
+    assert ['title', 'maturity', 'cub', 'young', 'adult', 'elder', 'Ranges', 'age_ranges'] == get_init_param()
+
+
+# def get_init_param():
+#     params = []
+#     pet = states.StateCalculator()
+#     for val in pet.create_new_creature().__dir__():
+#         if not val.startswith('__'):
+#             params.append(val)
 #
-#   for val in saved_pet:
-#     if not val.startswith('__'):
-#       new_atrs.append(val)
+#     return params
 #
-#   return new_atrs
-
-
-# Всё также на данный момент времени не разобрался с __eq__, поэтому тест, увы, такой. Как говорится: на безрыбье и
-# рак рыба
-# class TestActiveParameters:
-#   @staticmethod
-#   @mark.xfail
-#   @mark.parametrize('arg, exp_res', [({}, get_init_param()),
-#                                      ([], get_init_param()),
-#                                      ('', get_init_param()),
-#                                      (1, get_init_param()),
-#                                      (['unex_param','title', 'maturity', 'cub', 'young', 'adult', 'elder', 'Ranges',
-#                                        'age_ranges'], get_init_param()),
-#                                      (['maturity', 'cub', 'elder', 'Ranges',
-#                                        'age_ranges'], get_init_param())])
-#   def test_unhappy_keys(arg, exp_res):
-#     assert arg == exp_res
+# @mark.xfail
+# @mark.parametrize('arg, exp_res', [([], get_init_param()),
+#                                    ({}, get_init_param()),
+#                                    (['kind', 'name', '_Creature__birthdate', 'body', 'mind', 'age', 'tick_changes', 'feed', 'play',],
+#                                     get_init_param()),
+#                                    (1, get_init_param()),
+#                                    ('str', get_init_param()),
+#                                    (['unex_arg1', 'unex_arg2', 'kind', 'name', '_Creature__birthdate', 'body',
+#                                       'mind', 'age',
+#                                         'tick_changes', 'feed', 'play', 'talk', 'clean', 'action'], get_init_param())])
+# class TestCreature:
+#     @staticmethod
+#     def test_unhappy_creat_creature(arg, exp_res):
+#         assert arg == exp_res
 #
-#   @staticmethod
-#   def test_happy_keys():
-#     assert ['title', 'maturity', 'cub', 'young', 'adult', 'elder', 'Ranges', 'age_ranges'] == get_init_param()
-
-
-def get_init_param():
-    params = []
-    pet = states.StateCalculator()
-    for val in pet.create_new_creature().__dir__():
-        if not val.startswith('__'):
-            params.append(val)
-
-    return params
-
-@mark.xfail
-@mark.parametrize('arg, exp_res', [([], get_init_param()),
-                                   ({}, get_init_param()),
-                                   (['kind', 'name', '_Creature__birthdate', 'body', 'mind', 'age', 'tick_changes', 'feed', 'play',],
-                                    get_init_param()),
-                                   (1, get_init_param()),
-                                   ('str', get_init_param()),
-                                   (['unex_arg1', 'unex_arg2', 'kind', 'name', '_Creature__birthdate', 'body',
-                                      'mind', 'age',
-                                        'tick_changes', 'feed', 'play', 'talk', 'clean', 'action'], get_init_param())])
-class TestCreature:
-    @staticmethod
-    def test_unhappy_creat_creature(arg, exp_res):
-        assert arg == exp_res
-
-
-def test_happy_creat_creature():
-    assert ['kind', 'name', '_Creature__birthdate', 'body', 'mind', 'age', 'tick_changes', 'feed', 'play', 'talk', 'clean', 'action'] == get_init_param()
+#
+# def test_happy_creat_creature():
+#     assert ['kind', 'name', '_Creature__birthdate', 'body', 'mind', 'age', 'tick_changes', 'feed', 'play', 'talk', 'clean', 'action'] == get_init_param()
